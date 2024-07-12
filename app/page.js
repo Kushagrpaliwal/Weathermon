18e9b2aca042ac48067e9b2a697ab8aeef25e993
kushagr paliwal <97171186+Kushagrpaliwal@users.noreply.github.com>
2024-07-12 18:07:20 +0530

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,14 @@
+import  Main  from '@/Components/Main'
+import  Footer  from '@/Components/Footer'
+
+
+const page = () => {
+  return (
+    <div>
+    <Main/>
+    <Footer/>
+    </div>
+  )
+}
+
+export default page
